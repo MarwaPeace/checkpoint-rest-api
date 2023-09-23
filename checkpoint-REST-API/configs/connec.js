874991f886194ba7fb248486b6f79b533bb917b7
MarwaPeace <106271733+MarwaPeace@users.noreply.github.com>
2023-09-23 17:37:29 +0100

    Update connec.js

--- checkpoint-REST-API/configs/connec.js
+++ checkpoint-REST-API/configs/connec.js
@@ -2,7 +2,7 @@ const mongoose = require ('mongoose');
 
 const conndb = async()=>{
     try {
-        await mongoose.connect('mongodb+srv://ala:ala@cluster0.qctv87r.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect('mongodb+srv://marwa:marwa@cluster0.qctv87r.mongodb.net/?retryWrites=true&w=majority');
         console.log('we are connected to the world');
     } catch (error) {
        console.log(error); 
